e9b57826ee182816404d4c68f8b701d21e35722d
leopard3142 <39896829+leopard3142@users.noreply.github.com>
2020-04-02 00:23:58 -0300

    Arreglado el Leebinario, cambie "rb" por "r"

--- LeeBinario/main.c
+++ LeeBinario/main.c
@@ -5,11 +5,12 @@ int main(){
     long reg[16],ram[2000];
     FILE *Arch;
     int i;
-    Arch=fopen("imagenMemoria.img","rb");
+    Arch=fopen("imagenMemoria.img","r");
     if (Arch!=NULL)
     {
-        for (i=0; i<16; i++)
-            fread(&(reg[i]),sizeof(long),1,Arch);
+        for (i=0; i<16; i++){
+            fread(&(reg[i]),sizeof(long),1,Arch);}
+        i=0;
         while (fread(&(ram[i]),sizeof(long),1,Arch))
             i++;
     }
@@ -21,5 +22,5 @@ int main(){
     //ram[1]=0x0A;
     //ram[2]=0x20000020;
     for (i=0;i<19; i+=3)
-            printf("\n[%08X]\t\t\t%08X %08X %08X",i*3,ram[i*3],ram[i*3+1],ram[i*3+2]);
+            printf("\n[%08X]\t\t\t%08X %08X %08X",i,ram[i],ram[i+1],ram[i+2]);
 }
